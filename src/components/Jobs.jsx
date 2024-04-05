c7c5bf8ccf2a2ac2428f529abe34676e8420171c
Giriraj Singh Shekhawat <giriraj3103@gmail.com>
2024-04-05 19:06:39 +0530

    job and work with us' components animation done

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,30 +1,102 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { vacancies } from '../assets/data';
-
+import { motion } from 'framer-motion';
 function Jobs(props) {
+  const[onHover, setHover]=useState(false);
+  const [hoveredIndex, setHoveredIndex]=useState(null)
+
     return (
         <div>
               <div className='flex flex-col py-[50px] my-[50px]'>
-            <h1 className='text-4xl font-bold'>Open vacancies</h1>
-            <div className='flex gap-8 mt-12'>
-                {vacancies.map((job)=>(
-                     
-                      <div className="rounded-2xl bg-[#fefbec] p-6 px-12">
-          <h2 className="text-xl font-bold">{job.jobTitle}</h2>
+           <motion.div 
+           initial="hidden"
+           whileInView="visible"
+           viewport={{ always:true }}
+           transition={{ duration: 1.7,  type: "spring",bounce:0.4, ease: "easeInOut" }}
+           variants={{
+             visible: { opacity: 1, scale: 1, x: 0, y: 0 },
+             hidden: { opacity: 0, scale: 0.5, x: "-50%" },}}
+           >
+           <h1 className='text-4xl font-bold'>Open vacancies</h1>
+           </motion.div>
 
-          <ul className="mt-4 text-sm font-semibold list-disc pl-4">
-            <li className="text-sm font-normal mb-1">{job.position}</li>
-            <li className="text-sm font-normal mb-1">{job.location}</li>
-            <li className="text-sm font-normal">{job.pay}</li>
-          </ul>
-        </div>
+            <div className='flex gap-8 mt-12'>
+                {vacancies.map((job,index)=>{
+
+                
+              return      <HoveredCard key={index} index={index} jobs={job}/>
+              
+        //               <div className="rounded-2xl bg-[#fefbec] p-6 px-12">
+        //   <h2 className="text-xl font-bold">{job.jobTitle}</h2>
+
+        //   <ul className="mt-4 text-sm font-semibold list-disc pl-4">
+        //     <li className="text-sm font-normal mb-1">{job.position}</li>
+        //     <li className="text-sm font-normal mb-1">{job.location}</li>
+        //     <li className="text-sm font-normal">{job.pay}</li>
+        //   </ul>
+        // </div>
+        
                      
-                ))}
+            })}
+      
+            
+                
             
             </div>
            </div>
         </div>
     );
 }
+
+// hovered card component
+
+const HoveredCard=({index,jobs})=>{
+  const [showButton,setShowButton]=useState(false)
+
+  return(
+     <>
+     <motion.div
+     initial={{height:160}}
+     whileHover={{height:230}}
+     key={index}
+     onHoverStart={() => setShowButton(true)}
+     onHoverEnd={() => setShowButton(false)}
+    transition={{ ease: "easeInOut", duration: 0.2 }}
+     className='bg-[#fefbec] hover:bg-[#ece7d1] rounded-lg  cursor-pointer '
+    >
+               <div className="rounded-2xl p-6 px-12">
+          <h2 className="text-xl font-bold">{jobs.jobTitle}</h2>
+
+          <ul className="mt-4 text-sm font-semibold list-disc pl-4">
+            <li className="text-sm font-normal mb-1">{jobs.position}</li>
+            <li className="text-sm font-normal mb-1">{jobs.location}</li>
+            <li className="text-sm font-normal">{jobs.pay}</li>
+          </ul>
+        </div>
+
+        {showButton && 
+          <motion.button
+          initial="hidden"
+          whileInView="visible"
+          transition={{ ease: "easeInOut", duration: 0.3,delay:0.1  }}
+          variants={{
+            visible: { opacity: 1, scale: 1 },
+            hidden: { opacity: 0, scale: 0.7 },
+          }}
+          className="mx-12 cursor-pointer text-white bg-black rounded-full px-3 py-2 mt-3 "
+          >
+         Apply
+          </motion.button>
+        }
+
+     </motion.div>
+
+     </>
+  )
+
+}
+
+
+
 
 export default Jobs;
--- src/components/WorkWithUs.jsx
+++ src/components/WorkWithUs.jsx
@@ -1,20 +1,61 @@
 import React from 'react';
 import { WorkWithUsElementData } from '../assets/data';
+import { motion, spring } from 'framer-motion';
 
 function WorkWithUs(props) {
     return (
         <div className=' py-[10px] bg-[#f3f1ff] my-[50px] px-[30px] rounded-[23px]'>
    
            <div className='flex justify-between mb-[40px] mt-[70px]'>
+
+           <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ always:true }}
+            transition={{ duration: 2,  type: "spring",bounce:0.3 ,}}
+            variants={{
+              visible: { opacity: 1, scale: 1, x: 0, y: 0 },
+              hidden: { opacity: 0, scale: 0.5, x: "-50%" },
+            }}>
            <h1 className='font-[700] text-[43px] leading-[29px] font-sans mt-[12px]'>Work with us</h1>
+           </motion.div>
+            
+            <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{always:true}}
+            transition={{duration:2, type:"spring",bounce:0.3}}
+            variants={{
+                visible:{opacity:1, scale:1, x:0 , Y:0},
+                hidden:{opacity:0, scale:0.5, x:"50%"}
+            }}>
            <h1 className="text-[#5733f2]  font-[700] text-[45px] leading-[29px] font-sans mt-[12px] mr-12">ahead</h1>
+           </motion.div>
            </div>
+
            <div className='flex justify-between gap-10'>
         <div className='flex flex-col'>
            
             <div className="rounded-2xl bg-white  pt-5 w-[370px]  shadow-lg"> 
             <div  className="flex flex-col gap-3 px-4" >
-                <img src='src\assets\newGhost.png' alt='teddy Img' className='h-[70px] w-[60px] -ml-1'/>
+
+                <motion.div 
+                 initial="hidden"
+                 whileInView="visible"
+                 viewport={{ once: true }}
+                 transition={{ duration: 3, }}
+                 variants={{
+                    visible: { 
+                        opacity: [0.8,0.9,1], scale: [0.6,0.55,1],    rotate: [-10, 10, -10,0]  // Swinging left and right
+                    },
+                    hidden: { rotate: 0 },
+                  }}
+                       
+
+                 >
+                <img src='src\assets\newGhost.png' alt='teddy Img' className='h-[70px] w-[60px] -ml-1  '/>
+                </motion.div>
+                 
                 <h1 className="font-bold text-lg mt-1">About</h1>
                 <p className="font-normal text-base">At ahead our goal is to make self-improvement fun and lasting. We know there's a way how to make it work. And that's what ahead is all about!</p>
             </div>
