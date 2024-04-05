0959a21a5b3001cee6fde0283dc5ecd87e58ee14
Giriraj Singh Shekhawat <giriraj3103@gmail.com>
2024-04-05 23:26:42 +0530

    meetTheAhead component's animation done

--- src/components/MeetAheadApp.jsx
+++ src/components/MeetAheadApp.jsx
@@ -1,51 +1,227 @@
 import React from 'react';
-
+import { easeInOut, motion } from 'framer-motion';
 function MeetAheadApp(props) {
     return (
-        <div className='relative my-[50px]'> 
-        <div className='bg-[#fef7f1] flex py-[50px] h-[450px] rounded-[23px] overflow-hidden'>
+        <motion.div 
+         initial="hidden"
+         whileInView="visible"
+         viewport={{always:true}}
+         variants={{
+            hidden:{opacity:0.7, x:0, y:0, scaleY:0.9,} , 
+            visible:{opacity:1, x:0, y:0, scaleY:1 }
+         }}
+         transition={ { type: "spring",
+         bounce:0.4,
+         duration: "1.5",
+         ease: "easeInOut",
+         }}
+        className='relative my-[50px]'> 
+        <div className='bg-[#fef7f1] flex py-[50px] h-[450px] rounded-[23px]  overflow-hidden'>
             <div className='flex flex-col w-full  relative '>
             <span className='justify-start ml-[35px] mt-[50px]'>
+                <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{always:true}}
+                transition={{duration:1.7,type:"spring",bounce:0.4}}
+                variants={{
+                    visible: { opacity: 1, scale: 1, x: 0, y: 0 },
+                    hidden: { opacity: 0, scale: 0.5, x: "-20%" },
+                  }}
+                >
             <p className='text-sm font-bold  '>Built out of frustration</p>
             <h1 className='font-[700] text-[30px] leading-[29px] font-sans mt-[12px]'>Meet the ahead app</h1>
+            </motion.div>
             </span>
-            {/* circle element */}
-            <span className='absolute h-[200px] w-[200px] -left-[60px]  -bottom-[20px] bg-[#fae8dc] rounded-full '></span>
+       
+
+         {/* circle element */}
+            <motion.div
+  whileInView="visible"
+  viewport={{ always: true }}
+  initial="hidden"
+  variants={{
+    hidden: { opacity: 1, x:0, y: 200 },
+    visible: {
+      opacity: 1,x: [400, 0],  y: [50, 4],  
+       },
+  }}
+  transition={{
+    type: "tween",
+    duration: 1,
+    ease: "easeInOut",
+     
+  }}
+  className='absolute h-[200px] w-[200px] -left-[60px]  -bottom-[20px] bg-[#fae8dc] rounded-full '
+>
+</motion.div>
             
              {/* shaded circle element */}
-            <span className='absolute h-[10px] w-[10px] rounded-full bg-[#e9695b] ring-8  ring-[#f7d2bf] top-[300px] left-[260px]'></span>
+
+             <motion.div
+          whileInView="visible"
+          viewport={{ always: true }}
+          initial="hidden"
+          variants={{
+            visible: {
+              x: [190, 10],
+              y: [19, -20],
+              rotate: 25,
+            },
+            hidden: { y: 25 },
+          }}
+          
+          transition={{
+            type: "tween",
+            duration: 1.2,
+            ease: "easeInOut",
+            delay:0.3
+          }}
+          className='absolute h-[10px] w-[10px] rounded-full bg-[#e9695b] ring-8  ring-[#f7d2bf] top-[300px] left-[260px]'
+        >
              
+            </motion.div>
+
              {/* small circle element */}
-            <span className='absolute h-[25px] w-[25px] rounded-full  top-[200px] left-[310px] bg-[#f2c8c9]'></span>
+
+             <motion.div
+          whileInView="visible"
+          viewport={{ always: true }}
+          initial="hidden"
+          variants={{
+            visible: {
+              x: [190, 10],
+              y: [19, -20],
+              rotate: 25,
+            },
+            hidden: { y: 25 },
+          }}
+          
+          transition={{
+            type: "tween",
+            duration: 1.2,
+            ease: "easeInOut",
+            delay:0.3
+          }}
+          className='absolute h-[25px] w-[25px] rounded-full  top-[200px] left-[310px] bg-[#f2c8c9]'
+        >
+       </motion.div>
             {/* leaf element */}
-            <span className="  w-[50px] h-[30px] bg-[#75c496] overflow-hidden customleaf absolute left-[260px] top-[240px] -rotate-[40deg]"></span>
-           
+
+            <motion.div
+          whileInView="visible"
+          viewport={{ always: true }}
+          initial="hidden"
+          variants={{
+            visible: {
+              x: [190, 10],
+              y: [19, -20],
+              rotate: 360,
+            },
+            hidden: { y: 25 },
+          }}
+          
+          transition={{
+            type: "tween",
+            duration: 1.2,
+            ease: "easeInOut",
+            delay:0.3
+          }}
+          className="  w-[50px] h-[30px] bg-[#75c496] overflow-hidden customleaf absolute left-[260px] top-[240px] -rotate-[40deg]"
+        >
+       </motion.div>
+             
+            
             {/* sun element */}
-             <span className='h-[44px] w-[44px] absolute left-[150px] bottom-[150px] z-10'>
+            <motion.div
+            whileInView="visible"
+            viewport={{ always: true }}
+            initial="hidden"
+            variants={{
+              visible: { opacity: 1, scale: 1, y: 0,   },
+              hidden: { y: 60, opacity: 0, scale: 0.5 },
+            }}
+            transition={{
+              type: "spring",
+              bounce:0.4,
+              duration: "1.5",
+              ease: "easeInOut",
+              delay:0.3 ,
+            }}
+            className='h-[44px] w-[44px] absolute left-[150px] bottom-[150px] z-10'
+          >
                 <img src='src\assets\sun badge.webp' alt="sun image"/>
-             </span>
+
+             </motion.div>
             <div className=' absolute left-[100px] bottom-[30px]  h-[150px] w-[150px] rounded-full border-[10px] border-solid border-[#f4f6f6] bg-white'>
                 <img src='src\assets\TeddyImg2.svg' alt='teddyImg' className='  mix-blend-multiply'/>
             </div>
             </div>
             <div className='flex flex-col w-full justify-center items-start mt-[100px] mr-[50px]  '>
-                 {/* flower element */}
-                 <span className='h-[48px] w-[48px] absolute right-[56px] -top-[27px]   '>
-                    <img src='src\assets\flowerImg.webp' alt='flowerImg' />
-                 </span>
 
+
+                 {/* flower element */}
+                 <motion.div
+        whileInView="visible"
+        viewport={{ always: true }}
+        initial="hidden"
+        variants={{
+              visible: { opacity: 1,rotate:360 },
+              hidden: {  opacity: 1 },
+        }}
+  
+        transition={{
+          loop: Infinity, 
+          ease:  easeInOut,
+          duration: 1.9, 
+        }}
+        className="  h-[48px] w-[48px] absolute right-[56px] -top-[27px] "   
+      > 
+                 
+                    <img src='src\assets\flowerImg.webp' alt='flowerImg' />
+                 
+         </motion.div>
                 {/* leaf element */}
-            <span className="  w-[50px] h-[30px] bg-[#75c496] overflow-hidden customleaf absolute right-[100px] top-[50px] -rotate-[20deg]"></span>
-                
+
+                <motion.div
+        whileInView="visible"
+        viewport={{ always: true }}
+        initial="hidden"
+        variants={{
+              visible: { opacity: 1,rotate:360 },
+              hidden: {  opacity: 1 },
+        }}
+  
+        transition={{
+          loop: Infinity, 
+          ease:  easeInOut,
+          duration: 1.9, 
+        }}
+        className="  w-[50px] h-[30px] bg-[#75c496] overflow-hidden customleaf absolute right-[100px] top-[50px] -rotate-[20deg]"   
+      > 
+             
+            </motion.div>
+
+            <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{always:true}}
+                transition={{duration:1.7,type:"spring",bounce:0.3}}
+                variants={{
+                    visible: { opacity: 1, scale: 1, x: 0, y: 0 },
+                    hidden: { opacity: 0, scale: 0.5, x: "-20%" },
+                  }}
+                >
                 <p className='text-[16px] font-[400] leading-[23px]   text-black'>
                     A personalized pocket coach that provides bite-sized, science-driven tools to boost emotional intelligence.
                 </p>
                 <p className='text-[16px] font-[400] leading-[23px]   text-black mt-[10px]'>
                     Think of it as a pocket cheerleader towards a better, more fulfilling.
                 </p>
+                </motion.div>
             </div>
         </div>
-        </div>
+        </motion.div>
     );
 }
 
--- src/components/SelfImprovement.jsx
+++ src/components/SelfImprovement.jsx
@@ -1,31 +1,82 @@
 import React from 'react';
 import { selfImprovementElementData } from '../assets/data';
+import { motion } from 'framer-motion';
+import Reveal from "react-awesome-reveal";
+import { keyframes } from "@emotion/react"
 
 function SelfImprovement(props) {
+    const customAnimation = keyframes`
+    from {
+      opacity: 0.1;
+      transform: translate2d(-50px, 0);
+    
+    }
+  
+    to {
+      opacity: 1;
+      transform: translate2d(0, 0);
+    }
+  `;
+
     return (
         <div className=' flex flex-col py-[50px] my-[50px]'>
         <div className='ml-5 sm:ml-12 relative'>
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ always: true }}
+          transition={{ duration: 1.9, type: "spring", bounce: "0.1" }}
+          variants={{
+            visible: { opacity: 1, scale: 1, y: 0, x: 0 },
+            hidden: { opacity: 0, scale: 0, x: "-50%" },
+          }}
+        >
             <p className='text-sm font-[500]  '>Wrong with self-improvement & how we're fixing it.</p>
             <h1 className='font-[700] text-[35px] leading-[49px]   mt-[12px]'>Self-improvement. Ugh.</h1>
-            <img src='src\assets\blue-ghost.webp' alt='teddyImg' className='h-[70px] w-[70px]  absolute  top-5 left-[420px] rotate-1 '/>
+            </motion.div>
+
+            <motion.div
+          viewport={{ always: true }}
+          transition={{ duration: 2.1, ease: "easeInOut", type: "spring",delay:0.1 }}
+          intial="hidden"
+          whileInView="visible"
+          variants={{
+            visible: {
+              opacity: 1,
+              scale: 1,
+              x: [0, -100, -300, -450, -500],
+              y: [35, 20, 10, 0, -10],
+              rotate: [-60, -45, -35,-35,-35, -35, -15],
+            },
+            hidden: { opacity: 0, scale: 0},
+            
+          }}
+          className=' absolute   lg:top-[20px] lg:-right-[20px] rotate-1 '
+        >
+            <img src='src\assets\blue-ghost.webp' alt='teddyImg' className='h-[70px] w-[70px]      '/>
+       </motion.div>
         </div>
         <div className=' '>
         <div className='flex sm:justify-center ml-5 sm:ml-[0px]    sm:pl-[50px] mt-14 h-96 overflow--y-scroll  no-scrollbar  '>
             <ul className='flex flex-col border-l-2 border-l-violet-300  h-96 overflow-y-scroll   no-scrollbar   gap-10   '>
             
 
-            {
-                selfImprovementElementData.map((data)=>(
+               <Reveal keyframes={customAnimation}> 
+                {selfImprovementElementData.map((data,index)=>(
                    
-                    
+                    <motion.div
+                    key={index}
+                    className='relative  '> 
                     <li className='ml-6 relative '>
-                    <div   className="absolute -left-[35px]  ">🟣</div>
+                   
+                    <div   className="absolute -left-[35px] -top-[5px]  h-[8px] w-[8px] rounded-full  bg-violet-700 ring-8  ring-violet-400  "></div>
                     <h3 className='text-base font-bold mb-2 max-w-[500px]'>{data.title}</h3>
                     <p className='text-sm font-[500] max-w-[600px] '>{data.content}</p>
                    </li>
-                 
-                ))
-            }
+                   </motion.div>
+                ))}
+                </Reveal>
+             
             </ul>
         </div>
         </div>
@@ -34,3 +85,10 @@ function SelfImprovement(props) {
 }
 
 export default SelfImprovement;
+
+
+
+
+
+
+//absolute  top-5 left-[420px] rotate-1
